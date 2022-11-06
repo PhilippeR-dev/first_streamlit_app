dd484382578530f3a53cf50c99d24adf62bb50b4
Philippe REY <65810465+PhilippeR-dev@users.noreply.github.com>
2022-11-06 22:47:05 +0100

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add ?','Jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
-my_cur.execute("insert into fruit_load_list ( fruit_name) value ("+add_my_fruit+")")
+my_cur.execute("insert into fruit_load_list ( fruit_name) values ("+add_my_fruit+")")
 
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
