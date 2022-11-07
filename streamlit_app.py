0434a333e342f20d86a6a3818da6607ab0243ffe
Philippe REY <65810465+PhilippeR-dev@users.noreply.github.com>
2022-11-07 10:21:06 +0100

    Update streamlit_app.py
    clean

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,31 +67,4 @@ if streamlit.button('Add a Fruit to the List'):
   my_cnx.close()
   streamlit.text(back_from_function)
   
-streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_data_row = my_cur.fetchone()
-#streamlit.text("Hello from Snowflake:")
-#streamlit.text(my_data_row)
-
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-add_my_fruit = streamlit.text_input('What fruit would you like to add ?','Jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
-my_cur.execute("insert into fruit_load_list (fruit_name) values ('"+add_my_fruit+"')")
-
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-streamlit.write("thanks for adding ",add_my_fruit)
-
-my_cur.execute("insert into fruit_load_list (fruit_name) values ('from streamlit')")
-
-
+#streamlit.stop()
